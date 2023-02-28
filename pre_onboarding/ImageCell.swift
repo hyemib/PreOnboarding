7fea92be6e5c8d83c1ae5bc66a575264ff80f8c3
hyemib <hyem0127@gmail.com>
2023-02-28 20:27:31 +0900

    feat: UI Codebase로 변경

---
+++ pre_onboarding/ImageCell.swift
@@ -0,0 +1,76 @@
+//
+//  ImageCell.swift
+//  pre_onboarding
+//
+//  Created by hyemi on 2023/02/28.
+//
+
+import UIKit
+import SnapKit
+
+class ImageCell: UITableViewCell {
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [loadImageView, progressView, loadButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 3
+        
+        contentView.addSubview(stackView)
+        
+        stackView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(20)
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-15)
+            $0.bottom.equalToSuperview().offset(-15)
+        }
+        
+    
+        return stackView
+    }()
+    
+    lazy var loadImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "common"))
+        imageView.snp.makeConstraints {
+            $0.width.equalTo(130)
+            $0.height.equalTo(100)
+        }
+        return imageView
+    }()
+    
+    lazy var progressView: UIProgressView = {
+        let progressView = UIProgressView()
+        
+        progressView.progressViewStyle = .default
+        progressView.trackTintColor = .systemGray4
+        progressView.progressTintColor = .systemBlue
+        progressView.progress = 0.5
+        
+        return progressView
+    }()
+    
+    lazy var loadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Load", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        
+        button.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.height.equalTo(40)
+            
+        }
+        return button
+    }()
+   
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(stackView)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- pre_onboarding/ViewController.swift
+++ pre_onboarding/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView = UITableView()
+    private let loadAllButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        view.addSubview(loadAllButton)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalTo(UIEdgeInsets(top: 44, left: 0, bottom: 100, right: 0))
+        }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.separatorStyle = .none
+        tableView.isScrollEnabled = false
+        tableView.register(ImageCell.self, forCellReuseIdentifier: "imageCell")
+        
+        loadAllButton.setTitle("Load All Images", for: .normal)
+        loadAllButton.backgroundColor = .systemBlue
+        loadAllButton.layer.cornerRadius = 5
+
+
+        loadAllButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-30)
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+            $0.height.equalTo(45)
+        }
+        
+    
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as? ImageCell else { return .init() }
+        return cell
+    }
 }
 
